deb74c2b087b611cde08ac560b0e00e0c96887a2
tandisunarto <Spartan1170705>
2016-05-03 18:19:55 -0400

    Code refactoring

--- Learning_CSharp/Learnin_Async/MainForm.Designer.cs
+++ Learning_CSharp/Learnin_Async/MainForm.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -61,15 +62,26 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form1
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(466, 380);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -80,6 +92,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Learning_CSharp/Learnin_Async/MainForm.cs
+++ Learning_CSharp/Learnin_Async/MainForm.cs
@@ -12,55 +12,73 @@ namespace Learnin_Async
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            StudyCaseA();
-        }
+            listBox1.Items.Add($"{ShowDate()} Case A - getting message ... ");
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            StudyCaseB();
-        }
+            string message1 = await TaskReturnsStringAsync("Abby", 3);
+            string message2 = await TaskReturnsStringAsync("Bob", 2);
+            string message3 = await TaskReturnsStringAsync("Chris", 1);
 
-        private async void StudyCaseA()
-        {
-            listBox1.Items.Add($"{ShowDate()} getting message ... ");
-            string message1 = await GetMessagesAsync("Abby", 3);
-            string message2 = await GetMessagesAsync("Bob", 2);
-            string message3 = await GetMessagesAsync("Chris", 1);
             listBox1.Items.Add($"{ShowDate()} message received = {message1}");
             listBox1.Items.Add($"{ShowDate()} message received = {message2}");
             listBox1.Items.Add($"{ShowDate()} message received = {message3}");
             listBox1.Items.Add("---------------------------------------------");
         }
 
-        private async void StudyCaseB()
+        private async void button2_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Add($"{ShowDate()} getting message ... ");
-            Task<string> t1 = GetMessagesAsync("Abby", 3);
-            Task<string> t2 = GetMessagesAsync("Bob", 2);
-            Task<string> t3 = GetMessagesAsync("Chris", 1);
-            await t1;
-            await t2;
-            await t3;
+            listBox1.Items.Add($"{ShowDate()} Case B - getting message ... ");
+
+            Task<string> t1 = TaskReturnsStringAsync("Abby", 3);
+            Task<string> t2 = TaskReturnsStringAsync("Bob", 2);
+            Task<string> t3 = TaskReturnsStringAsync("Chris", 1);
+
+            await Task.WhenAll(t1, t2, t3);
+
             listBox1.Items.Add($"{ShowDate()} message received = {t1.Result}");
             listBox1.Items.Add($"{ShowDate()} message received = {t2.Result}");
             listBox1.Items.Add($"{ShowDate()} message received = {t3.Result}");
             listBox1.Items.Add("---------------------------------------------");
         }
 
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add($"{ShowDate()} Case C - doing long task ... ");
+            Task t = TaskReturnsNothingAsync();
+            await t;
+
+            listBox1.Items.Add($"{ShowDate()} Case C - finished long task ... ");
+            listBox1.Items.Add("---------------------------------------------");
+
+            listBox1.Items.Add("start DoSomethingAsync");
+            // DoSomethingAsync returns void (fire and forget function) so don't need to be await-ed.
+            TaskNotAwaitable();
+            listBox1.Items.Add("return from DoSomethingAsync");
+        }
+
+        private async Task<string> TaskReturnsStringAsync(string sender, int delay)
+        {
+            await Task.Run(() => Thread.Sleep(delay * 1000)); //or await Task.Delay(delay * 1000);
+            return $"Hello {sender}";
+        }
+
+        private Task TaskReturnsNothingAsync()
+        {
+            Task t = Task.Run(() => Thread.Sleep(2000));
+            return t;
+        }
+
+        private async void TaskNotAwaitable()
+        {            
+            await Task.Run(() => Thread.Sleep(2000));
+            listBox1.Items.Add("finished DoSomethingAsync");
+            listBox1.Items.Add("---------------------------------------------");
+        }
+
         private string ShowDate()
         {
             return $"{DateTime.Now.ToString("hh:mm:ss.fff")}";
-        }
-
-        private async Task<string> GetMessagesAsync(string sender, int delay)
-        {
-            //await Task.Delay(delay * 1000);
-            Task t = new Task(() => Thread.Sleep(delay * 1000));
-            t.Start();
-            await t;
-            return $"Hello {sender}";
         }
     }
 }
