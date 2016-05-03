6181dff0d1972979c2d373e8c2f369a4e8716622
tandisunarto <Spartan1170705>
2016-05-03 13:54:48 -0400

    Minor update

--- Learning_CSharp/Learnin_Async/MainForm.cs
+++ Learning_CSharp/Learnin_Async/MainForm.cs
@@ -24,29 +24,34 @@ namespace Learnin_Async
 
         private async void StudyCaseA()
         {
-            listBox1.Items.Add($"{DateTime.Now.ToString("hh:mm:ss.fff")} getting message ... ");
+            listBox1.Items.Add($"{ShowDate()} getting message ... ");
             string message1 = await GetMessagesAsync("Abby", 3);
             string message2 = await GetMessagesAsync("Bob", 2);
             string message3 = await GetMessagesAsync("Chris", 1);
-            listBox1.Items.Add($"{DateTime.Now.ToString("hh:mm:ss.fff")} message received = {message1}");
-            listBox1.Items.Add($"{DateTime.Now.ToString("hh:mm:ss.fff")} message received = {message2}");
-            listBox1.Items.Add($"{DateTime.Now.ToString("hh:mm:ss.fff")} message received = {message3}");
+            listBox1.Items.Add($"{ShowDate()} message received = {message1}");
+            listBox1.Items.Add($"{ShowDate()} message received = {message2}");
+            listBox1.Items.Add($"{ShowDate()} message received = {message3}");
             listBox1.Items.Add("---------------------------------------------");
         }
 
         private async void StudyCaseB()
         {
-            listBox1.Items.Add($"{DateTime.Now.ToString("hh:mm:ss.fff")} getting message ... ");
+            listBox1.Items.Add($"{ShowDate()} getting message ... ");
             Task<string> t1 = GetMessagesAsync("Abby", 3);
             Task<string> t2 = GetMessagesAsync("Bob", 2);
             Task<string> t3 = GetMessagesAsync("Chris", 1);
             await t1;
             await t2;
             await t3;
-            listBox1.Items.Add($"{DateTime.Now.ToString("hh:mm:ss.fff")} message received = {t1.Result}");
-            listBox1.Items.Add($"{DateTime.Now.ToString("hh:mm:ss.fff")} message received = {t2.Result}");
-            listBox1.Items.Add($"{DateTime.Now.ToString("hh:mm:ss.fff")} message received = {t3.Result}");
+            listBox1.Items.Add($"{ShowDate()} message received = {t1.Result}");
+            listBox1.Items.Add($"{ShowDate()} message received = {t2.Result}");
+            listBox1.Items.Add($"{ShowDate()} message received = {t3.Result}");
             listBox1.Items.Add("---------------------------------------------");
+        }
+
+        private string ShowDate()
+        {
+            return $"{DateTime.Now.ToString("hh:mm:ss.fff")}";
         }
 
         private async Task<string> GetMessagesAsync(string sender, int delay)
