2b532a696709b898b5507a231cf36bee972f043a
Sunarto, Tandi <tandi.sunarto@hotmail.com>
2018-04-09 16:49:17 -0400

    Add async console codes

--- Learning_CSharp/Async_console/Program.cs
+++ Learning_CSharp/Async_console/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Async_console
 {
@@ -6,10 +9,31 @@ namespace Async_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Async_01 async01 = new Async_01();
-            var info = async01.ShowTodaysInfo();
-            System.Console.WriteLine(info);
+            Console.WriteLine("Main Start");
+            GetContent();
+            Console.WriteLine("Main End");
+            Console.WriteLine(Environment.NewLine);
+        }
+
+        private static async void GetContent()
+        {
+            Console.WriteLine("GetContent() Start");
+            var http = new HttpWrapper();
+            var content = await http.GetUrlAsync("http://microsoft.com");
+            Console.WriteLine(Environment.NewLine + content);
+            Console.WriteLine("GetContent() End");
+        }
+    }
+
+    public class HttpWrapper
+    {
+        public async Task<string> GetUrlAsync(string url)
+        {
+            Console.WriteLine("HttpWrapper.GetUrlAsync() Start");
+            var httpClient = new HttpClient();
+            var content = await httpClient.GetStringAsync("http://microsoft.com");
+            Console.WriteLine("HttpWrapper.GetUrlAsync() End");
+            return content;
         }
     }
 }
