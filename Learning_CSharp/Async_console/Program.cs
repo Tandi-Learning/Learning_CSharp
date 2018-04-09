6642aee9395e3502c40a2ac27b07cdd79e956bb8
tandisunarto <tandi.sunarto@hotmail.com>
2018-04-09 11:41:39 -0400

    add async codes

--- Learning_CSharp/Async_console/Program.cs
+++ Learning_CSharp/Async_console/Program.cs
@@ -7,6 +7,9 @@ namespace Async_console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Async_01 async01 = new Async_01();
+            var info = async01.ShowTodaysInfo();
+            System.Console.WriteLine(info);
         }
     }
 }
