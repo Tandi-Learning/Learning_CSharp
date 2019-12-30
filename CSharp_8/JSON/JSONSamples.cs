33433a77e68293fbc8a8dda90c69d82000b5c366
tandisunarto <tandi.sunarto@hotmail.com>
2019-12-30 10:09:56 -0500

    Using Utf8JsonReader

---
+++ CSharp_8/JSON/JSONSamples.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using System.Text.Json;
+using static System.Console;
+
+namespace JSON
+{
+    public class JSONSample
+    {
+        public void Utf8JsonReader()
+        {
+            var data = File.ReadAllBytes("sample.json");        
+            var jsonReader = new Utf8JsonReader(data);
+
+            while (jsonReader.Read())
+            {
+                WriteLine(GetTokenDesc(jsonReader));
+            }
+        }
+
+        private string GetTokenDesc(Utf8JsonReader jsonReader)
+        {
+            return 
+            jsonReader.TokenType switch 
+            {
+                JsonTokenType.StartObject => "START OBJECT",
+                JsonTokenType.EndObject => "END OBJECT",
+                JsonTokenType.StartArray => "START ARRAY",
+                JsonTokenType.EndArray => "END ARRAY",
+                JsonTokenType.PropertyName => $"PROPERTY: {jsonReader.GetString()}",
+                JsonTokenType.Comment => $"COMMENT: {jsonReader.GetString()}",
+                JsonTokenType.String => $"STRING: {jsonReader.GetString()}",
+                JsonTokenType.Number => $"NUMBER: {jsonReader.GetInt32()}",
+                JsonTokenType.True => $"BOOL: {jsonReader.GetBoolean()}",
+                JsonTokenType.False => $"BOOL: {jsonReader.GetBoolean()}",
+                JsonTokenType.Null => "NULL",
+                _ => "UNIDENTIFIED"
+            };
+        }
+    }
+}
---
+++ CSharp_8/JSON/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static System.Console;
+
+namespace JSON
+{
+    class Program
+    {
+        static JSONSample jsonSample = new JSONSample();
+
+        static void Main(string[] args)
+        {
+            var key = ' ';
+            do {
+                ListOptions();
+                key = ReadKey(true).KeyChar;
+                ProcessResponse(key);
+
+            } while (key.ToString().ToLower() != "q");
+        }
+
+        static void ListOptions()
+        {
+            WriteLine();
+            WriteLine("C# 8 Pattern Matching");
+            WriteLine("-----------------------------");
+            WriteLine("1. Utf8JsonReader");
+            WriteLine("-----------------------------");
+            WriteLine("Q. Quit");
+            WriteLine();
+        }
+
+        static void ProcessResponse(Char key)
+        {
+            switch(key) 
+            {
+                case '1':
+                {
+                    jsonSample.Utf8JsonReader();
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
